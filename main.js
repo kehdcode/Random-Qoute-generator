62cc94335e16f4cb20ff48cc28faccd02dfa8a56
kehdcode <you@example.com>
2020-09-01 16:29:07 +0100

    function done using fetch api

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 
 function searchQoute(){
     console.log('hello')
-    let term = document.getElementById('search').value;
-    document.getElementById('display').innerHTML = term;
-    console.log(term);
-
+let display = document.getElementById('display')
+let author = document.getElementById('author')
+    fetch('http://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+        display.innerHTML = data.content;
+        author.innerHTML = data.author;
+    });
     
-
-    clearField();
-}
-
-
-
-function clearField(){
-    document.getElementById('search').value = '';
-
    
-    
+
+
 }
+
+
